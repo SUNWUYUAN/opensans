0fa342aa076b9b3a79065313d5f49a2e23ff5d64
SUNWUYUAN <88357633+SUNWUYUAN@users.noreply.github.com>
2022-07-16 14:37:26 +0800

    1

---
+++ sans.py
@@ -0,0 +1,54 @@
+import os
+
+# 读取文件
+if (os.path.isfile("www.txt")):
+    f = open('www.txt', 'r')
+    ls1 = f.readlines()
+    www = [s.strip() for s in ls1]
+    print(www)
+else:
+    print('文件不存在')
+    www = []
+
+if (os.path.isfile("name.txt")):
+    f = open('name.txt', 'r')
+    ls2 = f.readlines()
+    name = [s.strip() for s in ls2]
+    print(name)
+else:
+    print('文件不存在')
+    name = []
+
+# 添加输入的内容
+a = int(input('循环次数：'))
+for i in range(a):
+    name.append(input('输入名字：'))
+    www.append(input('输入网址：'))
+print(name)
+print(www)
+
+# 写入文件
+f = open('html.txt', 'w')
+for i in range(len(name)):
+    f.write('<v-btn class="ma-2" color="primary" href="%s">%s</v-btn>' %
+            (www[i], name[i]))
+print('写入html.txt文件成功')
+f.close()
+
+f = open('name.txt', 'w')
+for i in range(len(name)):
+    f.write(name[i])
+    f.write('\n')
+print('写入name.txt文件成功')
+
+f = open('www.txt', 'w')
+for i in range(len(www)):
+    f.write(www[i])
+    f.write('\n')
+print('写入www.txt文件成功')
+
+print('写入完成')
+
+
+#创建html文件
+f = open('index.html', 'w')
---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>网页版sans启动器</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/SUNWUYUAN/opensans" target="_blank" text>
+        <span class="mr-2">在GitHub上贡献</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <template>
+
+
+
+
+
+        <v-row justify="center">
+          <v-dialog v-model="dialog" persistent max-width="600">
+            <template v-slot:activator="{ on, attrs }">
+              <div class="fatherdiv">
+                <div class="sondiv">
+                  <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                    启动网页版sans模拟器
+                  </v-btn>
+                </div>
+              </div>
+            </template>
+
+
+            <v-card>
+              <v-toolbar color="primary" dark>启动网页版sans</v-toolbar>
+              <div>
+                <div style="height: 300px;overflow: auto;">
+                  <v-btn class="ma-2" color="primary" href="url1  ">
+                    sans1
+                  </v-btn>  
+
+                  <v-btn class="ma-2" color="primary" href="url2  ">
+                    sans2
+                  </v-btn>
+
+                  <v-btn class="ma-2" color="primary" href="url3  ">
+                    sans3
+                  </v-btn>
+                </div>
+              </div>
+              <v-card-actions class="justify-end">
+                <v-btn color="primary" text @click="dialog = false">
+                  取消
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-row>
+
+
+      </template>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+}
+</script>
+
+<style>
+.fatherdiv {
+
+  width: max-content;
+  height: max-content;
+}
+
+.sondiv {
+
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
